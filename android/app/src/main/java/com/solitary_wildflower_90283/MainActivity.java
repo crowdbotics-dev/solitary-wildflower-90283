fbd386d2b0e5a72a7f376f0bcf3c77745a64d53c
Crowdbotics <engineering@crowdbotics.com>
2023-06-10 23:13:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/solitary_wildflower_90283/MainActivity.java
@@ -0,0 +1,15 @@
+package com.solitary_wildflower_90283;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "solitary_wildflower_90283";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-wildflower-90283",
+  url: "https://solitary-wildflower-90283.botics.co",
+  api: "https://solitary-wildflower-90283.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
